c6f0d87cf3e63c888f94867779b7136a305f4603
ersaneris <erisersan@gmail.com>
2025-11-28 19:36:56 +0300

    Add: Particle Scene

---
+++ Assets/Scripts/ParticleSceneController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ParticleSceneController : MonoBehaviour
+{
+    public ParticleSystem CurrentParticle;
+    public SpriteRenderer GiantPinataSpriteRenderer;
+
+    private int _state;
+    public void OnPlayClick()
+    {
+        _state = ++_state%2;
+        switch (_state)
+        {
+            case 0:
+                CurrentParticle.Stop(true);
+                CurrentParticle.gameObject.SetActive(false);
+                GiantPinataSpriteRenderer.enabled = true;
+                break;
+            case 1:
+                CurrentParticle.gameObject.SetActive(true);
+                CurrentParticle.Play(true);
+                GiantPinataSpriteRenderer.enabled = false;
+                break;
+        }
+    }
+}
